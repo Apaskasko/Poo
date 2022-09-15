05e5be7818dd5aac6500a2eba874f69fffe42a12
Apaskasko <36697691+Apaskasko@users.noreply.github.com>
2022-09-15 16:26:45 -0300

    Update Media_Idades.java
    Correção

--- Lista Estrutura Repetição/Media_Idades.java
+++ Lista Estrutura Repetição/Media_Idades.java
@@ -1,28 +1,30 @@
-package lista3;
-
+import java.util.Locale;
 import java.util.Scanner;
 
-/* Faça um programa para ler um número indeterminado de dados, contendo cada um, a
-idade de um indivíduo. O último dado, que não entrará nos cálculos, contém um valor
-de idade negativa. Calcular e imprimir a idade média deste grupo de indivíduos. Se for
-entrado um valor negativo na primeira vez, mostrar a mensagem "IMPOSSIVEL
-CALCULAR".
-*/
+public class Ex2 {
+    public static void main(String[] args) throws Exception {
 
-public class Media_Idades {
-
-    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        int n = 0, count = 0;
-        double s = 0.0;
-        System.out.println("Digite as idades:");
+        int n = 0, contador = 0;
+        double soma = 0.0;
+        // while
+        System.out.println("Digite as idades: ");
         while (n >= 0) {
-            if (n >= 0)
-                s += n;
-            count++;
             n = sc.nextInt();
+            if (n >= 0) {
+                contador++;
+                soma += n;
+            } else if (contador == 0 && n < 1) {
+                System.out.println("impossivel Calcular");
+                contador = 0;
+                n = 0;
+            }
         }
-        System.out.println(s / (count - 1));
+        double media = soma / contador;
+        System.out.println(String.format("%.2f", media));
+
         sc.close();
     }
+
 }
